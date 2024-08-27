fc7aa09fa10f19228b107d15b4e800146359b267
imandrew26 <imandrew@umich.edu>
2024-08-27 01:22:43 -0400

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:new_ecommerce_app/components/bottom_nav_bar.dart';
+import 'cart_page.dart';
+import 'shop_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  //controls bottom nav bar
+   int _selectedIndex = 0;
+
+   void navigateBottomBar(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+   }
+
+   //pages to display
+   final List <Widget> _pages = [
+
+    const ShopPage(),
+    const CartPage()
+   ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Builder(
+          builder: (context) {
+            return IconButton(onPressed: (){
+              Scaffold.of(context).openDrawer();
+            },
+            icon: const Icon(Icons.menu));
+          }
+        ),
+      ),
+      drawer: Drawer(
+        backgroundColor: const Color.fromRGBO(33, 33, 33, 1),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                  Container(
+                    padding: const EdgeInsets.only(top: 25.0),
+                    height: 170,
+                    child: Image.asset(
+                      'lib/images/nike-logo.png',
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                    
+                  const Padding(
+                    padding: EdgeInsets.only(left: 25.0),
+                    child: ListTile(
+                      leading: Icon(Icons.home, color: Colors.white,),
+                      title: Text("Home",
+                      style: TextStyle( color: Colors.white),),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 25.0),
+                    child: ListTile(
+                      leading: Icon(Icons.info, color: Colors.white,),
+                      title: Text("About",
+                      style: TextStyle( color: Colors.white),),
+                    ),
+                  ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 25.0),
+                  child: ListTile(
+                    leading: Icon(Icons.person, color: Colors.white,),
+                    title: Text("Account",
+                    style: TextStyle( color: Colors.white),),
+                  ),
+                )
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 25.0),
+              child: ListTile(
+                leading: Icon(Icons.logout, color: Colors.white,),
+                title: Text("Logout",
+                style: TextStyle( color: Colors.white),),
+              ),
+            ),
+          ],
+        ),
+      
+      ),
+      body: _pages[_selectedIndex],
+    );
+  }
+}
---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  //logo
+                  Padding(
+                    padding: const EdgeInsets.all(25.0),
+                    child: Image.asset('lib/images/nike-logo.png', height: 240),
+                  ),
+                  const SizedBox(height: 30),
+
+                  //slogan
+                  const Text(
+                    "Just Do It",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20
+                    ),
+                  ),
+                const SizedBox(height: 30),
+          
+                //launch home
+                GestureDetector(
+                  onTap: () => Navigator.push(
+                    context, 
+                    MaterialPageRoute(
+                      builder: (context) => HomePage(),
+                    ),
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[900],
+                      borderRadius: BorderRadius.circular(10)
+                            
+                    ),
+                    padding: const EdgeInsets.all(20),
+                    child: const Center(
+                      child: Text(
+                        "Shop Now",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+                ]
+            )
+          ,),
+        )
+
+    );
+  }
+}
