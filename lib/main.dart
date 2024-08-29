1ca55093b0b86952c0b218ae3b7d6d718ef6c6ab
imandrew26 <imandrew@umich.edu>
2024-08-28 23:18:29 -0400

    backend prep

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:new_ecommerce_app/models/cart.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'pages/intro_page.dart';
 
-void main() {
+void main () async {
+  await dotenv.load(fileName: ".env");
+  Stripe.publishableKey = dotenv.env['STRIPE_PUBLISHABLE_KEY'] ?? '';
   runApp(const MyApp());
 }
 
